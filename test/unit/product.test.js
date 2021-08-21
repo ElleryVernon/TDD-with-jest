2a534d630d1aa55a021964991c943c679cce92c6
Ellery Vernon <tmpks3@gmail.com>
2021-08-21 19:10:28 +0900

    20210821

--- test/unit/product.test.js
+++ test/unit/product.test.js
@@ -1,1 +1,26 @@
-describe("Product Controller Create", () => {});
+const productController = require("../../controller/products");
+const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../data/new-product.json");
+
+productModel.create = jest.fn();
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
+describe("Product Controller Create", () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+  it("shoud have a createProduct function", () => {
+    expect(typeof productController.createProduct).toBe("function");
+  });
+  it("should call a productModel.create", () => {
+    productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(newProduct);
+  });
+});
